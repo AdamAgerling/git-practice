d63e04f2cc2ccb729d8c8cf2ba897807ba577696
Fredrik Strömberg <fredrikstromberg90@gmail.com>
2022-10-24 12:02:27 +0200

    Ffix issue

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with typescript bro. Hej brooo
+          Learn React with typescript bro. Hej bro
         </a>
       </header>
     </div>
